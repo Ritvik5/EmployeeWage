57f22dff6a20fea0d950f41e1c9edff231239082
Ritvik5 <sharma.ritvik56@gmail.com>
2023-02-04 22:59:55 +0530

    For Part Time Employee

--- empWage.sh
+++ empWage.sh
@@ -1,15 +1,18 @@
-#!/bin/bash 
+#!/bin/bash -x
 
 isPresent=1;
-randomCheck=$((RANDOM%2));
+isFullTime=2;
+empRatePerHr=20;
+randomCheck=$((RANDOM%3));
 
-if [ $isPresent -eq $randomCheck ];
+if [ $isFullTime -eq $randomCheck ];
 then
-	empRatePerHr=20
-	empHrs=8
-	salary=$(($empHrs*$empRatePerHr));
+	empHrs=8;
+elif  [ $isPartTime -eq $randomCheck ];
+then
+	empHrs=4;
 else
-	salary=0
+	empHrs=0;
 fi
 
-
+salary=$(($empHrs*$empRatePerHr));
